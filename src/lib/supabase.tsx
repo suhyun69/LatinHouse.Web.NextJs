378ca7ec10e7a25987886689e47652a15e520f59
suhyun.p69 <suhyun.p69@gmail.com>
2025-05-03 00:56:27 +0900

    supabase 수정

--- src/lib/supabase.tsx
+++ src/lib/supabase.tsx
@@ -1,6 +1,14 @@
+
+/*
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 ) 
+*/
+
+// lib/supabase.ts
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+
+export const supabase = createBrowserSupabaseClient()
