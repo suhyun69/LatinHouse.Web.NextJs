91ffb43b108bac54e4c6123703563ed42d4aecdb
suhyun.p69 <suhyun.p69@gmail.com>
2025-05-27 12:43:22 +0900

    로그 추가

--- middleware.ts
+++ middleware.ts
@@ -11,6 +11,8 @@ export async function middleware(req: NextRequest) {
     data: { session },
   } = await supabase.auth.getSession()
 
+  console.log("session", session)
+
   if (!session && req.nextUrl.pathname.startsWith('/lessonMng')) {
     return NextResponse.redirect(new URL('/', req.url))
   }
